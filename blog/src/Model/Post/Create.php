734ec81a5f9115b4d1db2a25b6c99ecd0405ad18
jonaprg <jonargp1@gmail.com>
2021-11-12 23:48:17 +0100

    Refactor Test - Create

--- blog/src/Model/Post/Create.php
+++ blog/src/Model/Post/Create.php
@@ -12,7 +12,30 @@ class Create
     }
 
     public function create(Post $post) : bool {
+        $postByID =  $this->getPostByID($post->getId());
+        if(!$postByID){
+            $this->insert($post);
+            return (bool) $this->getPostByID($post->getId());
+        }
         return false;
+
+    }
+
+    private function getPostByID(string $id) {
+        if(empty($id)) return null;
+        $query = $this->connection->prepare('SELECT id, idUser, title FROM posts WHERE id = :id');
+        $query->bindValue(':id', $id);
+        $query->execute();
+        return $query->fetch(PDO::FETCH_ASSOC) ?? null;
+    }
+
+    private function insert(Post $post) {
+        $query = $this->connection->prepare('INSERT INTO posts (id, subject, message, id_user) VALUES (:user_id, :subject, :message, :id_post)');
+        $query->bindValue(':id', $post->getId());
+        $query->bindValue(':subject', $post->getSubject());
+        $query->bindValue(':message', $post->getMessage());
+        $query->bindValue(':id_user', $post->getUserId());
+        $query->execute();
     }
 
 
--- blog/tests/Model/Post/CreateTest.php
+++ blog/tests/Model/Post/CreateTest.php
@@ -7,48 +7,21 @@ use App\Model\Post\Post;
 use PDO;
 use PDOStatement;
 use PHPUnit\Framework\TestCase;
+use Tests\PDOStatementMock;
 
 class CreateTest extends TestCase
 {
     public function testCreatePostWithCorrectDataExpectsTrue() {
-        $pdoStatementMock = $this->getMockBuilder(PDOStatement::class)
-            ->setMethods(array('bind','execute'))
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $pdoMock = $this->getMockBuilder(PDO::class)
-            ->setMethods(array('prepare'))
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $pdoMock->expects($this->any())
-            ->method('prepare')
-            ->willReturn($pdoStatementMock);
-
-
-        $createPost = new Create($pdoMock);
+        $createPost = new Create((new PDOStatementMock)->create([], ['id' => '1',
+            'idUser'=>'1', 'title' => 'Title']));
         $post = new Post("1", "Title", "12345678901234567890", "1");
         $this->assertTrue($createPost->create($post));
-
     }
 
-    public function testCreatePostWithInvalidDataExpectsFalse() {
-        $pdoStatementMock = $this->getMockBuilder(PDOStatement::class)
-            ->setMethods(array('bind','execute'))
-            ->disableOriginalConstructor()
-            ->getMock();
+    public function testCreatePostWithExistsIDInBDExpectsFalse() {
 
-        $pdoMock = $this->getMockBuilder(PDO::class)
-            ->setMethods(array('prepare'))
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $pdoMock->expects($this->never())
-            ->method('prepare')
-            ->willReturn($pdoStatementMock);
-
-        $createPost = new Create($pdoMock);
-        $post = new Post("", "Title", "12345678901234567890", "");
+        $createPost = new Create((new PDOStatementMock)->create([]));
+        $post = new Post("", "Title", "12345678901234567890", "Hola");
         $this->assertFalse($createPost->create($post));
     }
 }
