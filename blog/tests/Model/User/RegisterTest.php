8494020ccf85f3c17b648fec42e50c24b2f83a43
estufamagica <57752904+estufamagica@users.noreply.github.com>
2021-11-12 02:07:10 +0100

    Red test testNameEmailPasswordCorrectsExpectsCorrect testEmptyNameExpectsIncorrectRegister testIncorrectPasswordVerifyExpectsIncorrectRegister testIncorrectCharactersInNameExpectsIncorrectRegister testCorrectCharactersInNameExpectsCorrectRegister testTwentyCharactersNameExpectsCorrectRegister testTwentyOneCharactersNameExpectsIncorrectRegister testNineTeenCharactersNameExpectsCorrectRegister

--- blog/tests/Model/User/RegisterTest.php
+++ blog/tests/Model/User/RegisterTest.php
@@ -18,6 +18,58 @@ class RegisterTest extends TestCase
     }
 
     public function testNameEmailPasswordCorrectsExpectsCorrect(){
+        $register = new Register((new \Tests\PDOStatementMock)->create([]));
 
+        $user = new User("test@gmail.com", "12345678");
+        $this->assertTrue($register->register($user, "admin", "12345678"));
+    }
+
+    public function testEmptyNameExpectsIncorrectRegister(){
+        $register = new Register((new \Tests\PDOStatementMock)->create([]));
+
+        $user = new User("test@gmail.com", "12345678");
+        $this->assertFalse($register->register($user, "", "12345678"));
+    }
+
+    public function testIncorrectPasswordVerifyExpectsIncorrectRegister(){
+        $register = new Register((new \Tests\PDOStatementMock)->create([]));
+
+        $user = new User("test@gmail.com", "12345678");
+        $this->assertFalse($register->register($user, "admin", "afafajafaf"));
+    }
+
+    public function testIncorrectCharactersInNameExpectsIncorrectRegister(){
+        $register = new Register((new \Tests\PDOStatementMock)->create([]));
+
+        $user = new User("test@gmail.com", "12345678");
+        $this->assertFalse($register->register($user, "a!_::3HJ/", "12345678"));
+    }
+
+    public function testCorrectCharactersInNameExpectsCorrectRegister(){
+        $register = new Register((new \Tests\PDOStatementMock)->create([]));
+
+        $user = new User("paco2399@gmail.com", "paquito24");
+        $this->assertTrue($register->register($user, "PacoX23", "paquito24"));
+    }
+
+    public function testTwentyCharactersNameExpectsCorrectRegister(){
+        $register = new Register((new \Tests\PDOStatementMock)->create([]));
+
+        $user = new User("test@gmail.com", "12345678");
+        $this->assertTrue($register->register($user, "12345678901234567890", "12345678"));
+    }
+
+    public function testTwentyOneCharactersNameExpectsIncorrectRegister(){
+        $register = new Register((new \Tests\PDOStatementMock)->create([]));
+
+        $user = new User("test@gmail.com", "12345678");
+        $this->assertFalse($register->register($user, "123456789012345678901", "12345678"));
+    }
+
+    public function testNineTeenCharactersNameExpectsCorrectRegister(){
+        $register = new Register((new \Tests\PDOStatementMock)->create([]));
+
+        $user = new User("test@gmail.com", "12345678");
+        $this->assertTrue($register->register($user, "1234567890123456789", "12345678"));
     }
 }
