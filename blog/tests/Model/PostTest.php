06df5f54f07bc3c54699681a83f12b1038a6df35
jonaprg <jonargp1@gmail.com>
2021-11-12 18:25:24 +0100

    Red Test - testCreatePostWithTwentyOneCharactersSubjectExpectsException

--- blog/tests/Model/PostTest.php
+++ blog/tests/Model/PostTest.php
@@ -18,5 +18,12 @@ class PostTest extends TestCase
         $this->assertFalse($post->create(), "Invalid Subject");
     }
 
+    public function testCreatePostWithTwentyOneCharactersSubjectExpectsException(){
+
+        $post = new Post("123456789012345678901", "Hola Pepito", "id_1");
+        $this->assertFalse($post->create(), "Invalid Subject");
+    }
+
+
 
 }
