fe0c21c9f5fe8dc0385ef10014e5b68c9ea4698e
jonaprg <jonargp1@gmail.com>
2021-11-12 20:29:38 +0100

    Green Test - testPostWithTwentyCharactersInMessageExpectsCorrect

--- blog/src/Model/Post/Post.php
+++ blog/src/Model/Post/Post.php
@@ -7,6 +7,7 @@ class Post
     private const SUBJECT_MIN_LENGTH = 3;
     private const SUBJECT_MAX_LENGTH = 21;
     private const MESSAGE_MAX_LENGTH = 257;
+    private const MESSAGE_MIN_LENGTH = 19;
     private string $subject;
     private string $message;
     private string $user_id;
@@ -58,7 +59,7 @@ class Post
     }
 
     private function isCorrectMessage(string $message): bool {
-        return strlen($message) < self::MESSAGE_MAX_LENGTH ;
+        return strlen($message) > self::MESSAGE_MIN_LENGTH && strlen($message) < self::MESSAGE_MAX_LENGTH ;
     }
 
     private function isEmptyMessage(string $message): bool {
